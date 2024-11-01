6dc18e768592eeddd5c0694acafeab4b9f6451c4
THORIQ DZAKI UAD <2300018216@webmail.uad.ac.id>
2024-11-01 20:26:48 +0700

    Update LinkedList.cpp

--- Linked list.cpp
+++ Linked list.cpp
@@ -1,40 +1,109 @@
 #include <iostream>
 using namespace std;
 
+// Deklarasi struct
 struct pnt {
     char data;
-    pnt* left;   // Cabang kiri
-    pnt* right;  // Cabang kanan
+    pnt* next;
 };
 
+// Fungsi untuk menambah data di depan
+void tambahDepan(pnt*& kpl, char dataBaru) {
+    pnt* baru = new pnt();
+    baru->data = dataBaru;
+    baru->next = kpl;
+    kpl = baru;
+}
+
+// Fungsi untuk menambah data di belakang
+void tambahBelakang(pnt*& ekor, char dataBaru) {
+    pnt* baru = new pnt();
+    baru->data = dataBaru;
+    baru->next = nullptr;
+    ekor->next = baru;
+    ekor = baru;
+}
+
+// Fungsi untuk menyisipkan data di tengah
+void tambahTengah(pnt* kpl, char dataBaru) {
+    pnt* baru = new pnt();
+    baru->data = dataBaru;
+    baru->next = kpl->next;
+    kpl->next = baru;
+}
+
+// Fungsi untuk menghapus data di depan
+void hapusDepan(pnt*& kpl) {
+    if (kpl != nullptr) {
+        pnt* tmp = kpl;
+        kpl = kpl->next;
+        delete tmp;
+    }
+}
+
+// Fungsi untuk menghapus data di belakang
+void hapusBelakang(pnt*& kpl) {
+    if (kpl != nullptr) {
+        pnt* prev = nullptr;
+        pnt* curr = kpl;
+        while (curr->next != nullptr) {
+            prev = curr;
+            curr = curr->next;
+        }
+        if (prev != nullptr) {
+            prev->next = nullptr;
+        } else {
+            kpl = nullptr; // Jika hanya ada satu elemen
+        }
+        delete curr;
+    }
+}
+
+// Fungsi untuk menampilkan isi linked list
+void tampilkanList(pnt* kpl) {
+    pnt* tmp = kpl;
+    while (tmp != nullptr) {
+        cout << tmp->data << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
 int main() {
-    // Alokasi memori untuk node root (kepala)
-    pnt* kpl = new pnt;
-    kpl->data = 'a';     // Isi data root dengan 'a'
-    kpl->left = nullptr; // Set left dan right menjadi null
-    kpl->right = nullptr;
-
-    // Node tengah dengan data 'C'
-    pnt* tengah = new pnt;
-    tengah->data = 'C';   // Isi data node tengah dengan 'C'
-    tengah->left = nullptr;
-    tengah->right = nullptr;
-
-    // Node baru dengan data 'B'
-    pnt* baru = new pnt;
-    baru->data = 'B';     // Isi data node baru dengan 'B'
-    baru->left = nullptr;
-    baru->right = nullptr;
-
-    // Menghubungkan node untuk membentuk pohon biner
-    kpl->left = tengah;  // Tengah berada di cabang kiri root
-    kpl->right = baru;   // Baru berada di cabang kanan root
-
-    // Menampilkan data dari setiap node di pohon biner
-    cout << "Root: " << kpl->data << endl;
-    cout << "Left Child of Root: " << kpl->left->data << endl;
-    cout << "Right Child of Root: " << kpl->right->data << endl;
-
+    pnt* kpl = new pnt();  // Kepala
+    pnt* ekor = new pnt(); // Ekor
+    pnt* baru = nullptr;   // Node baru
+    
+    // Inisialisasi kpl dengan 'A'
+    kpl->data = 'A';
+    kpl->next = nullptr;
+    ekor = kpl;
+    
+    cout << "Data kpl: " << kpl->data << endl;
+    cout << "Data ekor: " << ekor->data << endl;
+    
+    // Tambah data B di belakang
+    tambahBelakang(ekor, 'B');
+    
+    // Tambah data C di depan
+    tambahDepan(kpl, 'C');
+    
+    // Sisip data D di tengah
+    tambahTengah(kpl->next, 'D'); // Sisip setelah elemen pertama
+    
+    // Tampilkan isi linked list
+    cout << "Isi linked list: ";
+    tampilkanList(kpl);
+    
+    // Hapus data depan
+    hapusDepan(kpl);
+    
+    // Hapus data belakang
+    hapusBelakang(kpl);
+    
+    // Tampilkan isi linked list setelah penghapusan
+    cout << "Isi linked list setelah hapus depan dan belakang: ";
+    tampilkanList(kpl);
+    
     return 0;
-
 }
